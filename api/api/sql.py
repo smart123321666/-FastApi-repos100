7eb0289fb1646240a145e5e24e60d7804073fd67
Даниил Соколов <scake14@yandex.ru>
2024-02-11 23:34:10 +0500

    res

--- api/api/sql.py
+++ api/api/sql.py
@@ -1,9 +1,7 @@
 import os
 
 import psycopg2
-import os
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
--- api/parser/github_parser.py
+++ api/parser/github_parser.py
@@ -3,6 +3,7 @@ import os
 import psycopg2
 import requests
 from dotenv import load_dotenv
+
 load_dotenv()
 
 DB_HOST = os.getenv('DB_HOST')
@@ -13,10 +14,12 @@ DB_DATABASE = os.getenv('DB_NAME')
 
 URL_API = os.getenv('URL_API')
 
+
 def get_data():
     response = requests.get(URL_API)
     data = response.json()
     return data
+
 
 def save_data(data):
     try:
@@ -31,12 +34,19 @@ def save_data(data):
 
         for item in data:
             cur.execute(
-                'INSERT INTO repositories (repo, owner, position_cur, position_prev, stars, watchers, forks, open_issues, language) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)',
-                (item['repo'], item['owner'], item['position_cur'], item['position_prev'], item['stars'], item['watchers'], item['forks'], item['open_issues'], item['language'])
+                '''INSERT INTO repositories (repo, owner,
+                position_cur, position_prev, stars, watchers,
+                forks, open_issues, language) VALUES
+                (%s, %s, %s, %s, %s, %s, %s, %s, %s)''',
+                (item['repo'], item['owner'],
+                 item['position_cur'], item['position_prev'],
+                 item['stars'], item['watchers'],
+                 item['forks'], item['open_issues'], item['language'])
             )
             repo_id = cur.lastrowid
             cur.execute(
-                'INSERT INTO commits (repo_id, date, commits, authors) VALUES (%s, %s, %s, %s)',
+                '''INSERT INTO commits (repo_id, date, commits, authors)
+                VALUES (%s, %s, %s, %s)''',
                 (repo_id, item['date'], item['commits'], item['authors'])
             )
 
